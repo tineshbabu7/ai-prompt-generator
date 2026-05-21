3320b9a07efc8648dcf720cbf828b14b2cf639dc
tineshbabu7 <tineshbabu7@gmail.com>
2026-05-21 10:20:15 +0530

    Fixed error cors

--- backend/main.py
+++ backend/main.py
@@ -22,16 +22,11 @@ app = FastAPI(title="Promptomania API", version="2.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5500",
-        "http://127.0.0.1:5500",
-        "https://ai-prompt-generator-66twu62lg-tineshbabu7-4659s-projects.vercel.app"
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 @app.get("/")
 def root():
